30fea6796bed67f0497bf73e9a2051dc8ccf18c4
brucechanjianle <jianle001@e.ntu.edu.sg>
2023-05-31 17:33:22 +0800

    feat: add common aliases for zsh

---
+++ aliases.zsh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This script stores aliases that are frequently used.
+
+# Common
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
