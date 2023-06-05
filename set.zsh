09acb253fb9e62880e5f2cfb740622186caa314d
brucechanjianle <jianle001@e.ntu.edu.sg>
2023-06-05 21:45:36 +0800

    use vim keys for menu-select

--- set.zsh
+++ set.zsh
@@ -11,13 +11,13 @@ _comp_options+=(globdots)
 
 # Vim style menu select
 # ref: https://thevaluable.dev/zsh-install-configure-mouselessk
-# bindkey -M menuselect 'h' vi-backward-char
-# bindkey -M menuselect 'k' vi-up-line-or-history
-# bindkey -M menuselect 'l' vi-forward-char
-# bindkey -M menuselect 'j' vi-down-line-or-history
-# bindkey -M menuselect '^y' accept-line
-# bindkey -M menuselect '^l' accept-line
-# bindkey -M menuselect '^e' send-break
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect '^y' accept-line
+bindkey -M menuselect '^l' accept-line
+bindkey -M menuselect '^e' send-break
 
 # History in cache directory:
 HISTSIZE=10000
