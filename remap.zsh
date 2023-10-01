e14c6f6de6b8238183e00839f64700f70195804d
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2023-10-01 11:53:21 +0800

    fix: broken sessionizer
    Use \r for caret return symbol instead of \n.

--- remap.zsh
+++ remap.zsh
@@ -12,7 +12,7 @@ bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect '^y' accept-line
 bindkey -M menuselect '^l' accept-line
 bindkey -M menuselect '^e' send-break
-bindkey -s '^f' "tmux-sessionizer\n"
+bindkey -s '^f' "tmux-sessionizer\r"
 bindkey -s '^j' "hyperjump\r"
 
 # Get current prompt text
