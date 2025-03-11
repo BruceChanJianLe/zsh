10573a37be79793abd110737406733651c6ae58e
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-03-11 14:19:13 +0800

    fix: typo

--- set.zsh
+++ set.zsh
@@ -27,8 +27,8 @@ HISTFILE=~/.cache/zsh/history
   [ -n "$PS1" ] && \
     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
     source "$BASE16_SHELL/profile_helper.sh"
-  export BASE16_THEME=gruvbox-dark-medium
-  gruvbox-dark-medium
+  base16_gruvbox-dark-medium
+  # export BASE16_THEME=gruvbox-dark-medium
 # fi
 
 # Disable paste highlight
